7a378f3f6b96497ac820929e0141020d0ac9309d
sandipghosh <sandipgh19@gmail.com>
2017-07-04 17:00:35 +0530

    modify code

--- Saundrya/src/app/main.ts
+++ Saundrya/src/app/main.ts
@@ -1,15 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
-import { HttpModule } from '@angular/http';
-
-@NgModule({
-    imports: [ BrowserModule, HttpModule],
-    providers: [],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
-})
-export default class AppModule { }
-
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- Saundrya/src/pages/register/register.ts
+++ Saundrya/src/pages/register/register.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { Http, Headers, RequestOptions } from '@angular/http';
-
-
 /**
  * Generated class for the Register page.
  *
@@ -15,40 +12,13 @@ import { Http, Headers, RequestOptions } from '@angular/http';
   selector: 'page-register',
   templateUrl: 'register.html',
 })
-
-
 export class Register {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
-
-  this.http = http;
-
-  	
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Register');
-  }
-
-  register() {
-
-  var headers = new Headers();
-    headers.append("Accept", 'application/json');
-    headers.append('Content-Type', 'application/json' );
-    let options = new RequestOptions({ headers: headers });
- 
-    let postParams = {
-      title: 'foo',
-      body: 'bar',
-      userId: 1
-    }
-    
-    this.http.post("http://sandip.com/", postParams, options)
-      .subscribe(data => {
-        console.log(data['_body']);
-       }, error => {
-        console.log(error);// Error getting the data
-      });
   }
 
 }
