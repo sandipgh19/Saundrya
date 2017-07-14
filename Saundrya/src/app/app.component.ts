b4f466d37e75519bba5d5e0aef957d66961db52d
sandipghosh <sandipgh19@gmail.com>
2017-07-14 21:15:39 +0530

    Profile Page

--- Saundrya/src/app/app.component.ts
+++ Saundrya/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { Service } from '../pages/service/service';
 import { Profile } from '../pages/profile/profile';
+import { Storage } from '@ionic/storage';
 @Component({
   templateUrl: 'app.html'
 })
--- Saundrya/src/pages/profile/profile.ts
+++ Saundrya/src/pages/profile/profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the Profile page.
@@ -13,12 +14,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html',
 })
 export class Profile {
+	email: any;
+	name: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Profile');
+
+     this.storage.get('email').then((val) => {
+    	this.email = val;
+  });
+
+  	this.storage.get('name').then((val) => {
+    	this.name = val;
+  	});
+  }
   }
 
 }
