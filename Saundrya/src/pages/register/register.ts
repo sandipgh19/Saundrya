30d9fb50166311edf8f33171a89b89da7488bcb7
sandipghosh <sandipgh19@gmail.com>
2017-07-16 23:37:06 +0530

    Register Page

--- Saundrya/src/pages/register/register.ts
+++ Saundrya/src/pages/register/register.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 
 /**
@@ -17,8 +18,16 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class Register {
 
 	public registrationForm:any;
+  name: any;
+  email: any;
+  password: any;
+  mobile: any;
+  street: any;
+  city: any;
+  zip: any;
+  state: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public _form:FormBuilder) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _form:FormBuilder, private loadingCtrl: LoadingController, public http: Http) {
 
   	this.registrationForm = this._form.group({
 
@@ -37,6 +46,39 @@ export class Register {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Register');
+  }
+
+  register() {
+
+  console.log(this.email);
+    let loader = this.loadingCtrl.create({
+      content: "Register..."
+    });
+    loader.present();
+ 
+    let postParams = JSON.stringify({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      mobile: this.mobile,
+      street: this.street,
+      city: this.city,
+      zip: this.zip,
+      state: this.state
+
+    });
+    
+    this.http.post("", postParams).map(res => res.json())
+      .subscribe(data => {
+        console.log(data['_body']);
+        console.log(data.message);
+        loader.dismiss();
+        alert(data.message);
+       }, error => {
+        console.log(error);// Error getting the data
+      });
+
+
   }
 
 }
